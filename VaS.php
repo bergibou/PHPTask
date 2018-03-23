56aa20c33c3de02b4da12df57bcae5857cbd25ea
Ganjibay <tebbalsamy@gmail.com>
2018-03-23 14:39:56 +0100

    Update VaS.php

--- VaS.php
+++ VaS.php
@@ -1,19 +1,9 @@
 <!DOCTYPE html>
-  <html>
+  <html lang="en">
     <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <style>
-body {
-    background-color: #42ff004f;
-}
-</style>
-                
+      <?php
+                require 'nav.php';
+            ?>    
     </head>
 
     <body>
