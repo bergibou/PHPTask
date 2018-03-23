5e8bdb96731cacf61424ccd264fc33a8ed45f635
Ganjibay <tebbalsamy@gmail.com>
2018-03-23 14:35:55 +0100

    Update HP.php

--- HP.php
+++ HP.php
@@ -2,20 +2,9 @@
 <html>
     <head>
 
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,all"/>
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <link rel="shortcut icon" href="assets/Logo.ico">
-      <title>Gallery</title>
-        <style>
-body {
-    background-color: lightgreen;
-}
-</style>
+       <?php
+                require 'header.php';
+            ?>
     </head>
 
     <body>
