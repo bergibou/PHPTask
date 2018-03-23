7526c4b5a61f8760accf5fb383aae79725e0f8f5
Ganjibay <tebbalsamy@gmail.com>
2018-03-23 14:35:15 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,all"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="shortcut icon" href="assets/Logo.ico">
+      <title>Gallery</title>
+        <style>
+body {
+    background-color: lightgreen;
+}
+</style>
