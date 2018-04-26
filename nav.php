82775a77799519fccc4718ec50bfefa0955dc8ba
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 00:29:49 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="VaS.php">View and Search</a></li>
         <li><a href="Login.php">Sign in</a></li>
-        <li><a href="">Sign up</a></li>
+        <li><a href="Signup.php">Sign up</a></li>
       </ul>
     </div>
   </nav>
