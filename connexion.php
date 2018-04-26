ebe8c7a88c64a3c94fa4bf09c86d91a7821342ad
Ganjibay <tebbalsamy@gmail.com>
2018-04-26 21:45:01 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "materialdb";
+$dbname = "samy_";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
