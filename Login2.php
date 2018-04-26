6356684e768e535d3990a68d190b7013b7ee5482
Ganjibay <tebbalsamy@gmail.com>
2018-04-26 23:44:45 +0200

    Create Login2.php

---
+++ Login2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <?php
+                require 'headerlog.php';
+            ?>
+    </head>
+
+    <body>
+        <header>
+      <?php
+                require 'nav.php';
+            ?>
+      
+        
+            </header>
+        <center><h2>Login</h2>
+
+<form method="post" action="AR.php">
+  <div class="imgcontainer">
+    <img src="assets/images.png" alt="Avatar" class="avatar">
+    </div></center>
+  <div class="container">
+    <label for="pseudo"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="pseudo" id="pseudo" required>
+
+    <label for="password"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" id"password" required>
+        
+    <button type="submit">Login</button>
+    Materialize.toast('I am a toast!', 4000)
+  </div>
+</form>
+      <?php 
+	    
+  require 'connexion.php';
+  if (!isset($_POST['submit']))
+{
+	$retour = mysqli_query($link ,'SELECT customer_id FROM customers WHERE customer_name = :pseudo AND customer_password = :password');	
+} 
+$conn->close();
+ require 'footer.php';
+	    
+            ?>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
