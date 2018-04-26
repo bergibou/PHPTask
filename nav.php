355ebe2d43d16319d77ac627aa063dd9d8acec11
Ganjibay <tebbalsamy@gmail.com>
2018-04-26 22:16:23 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -3,8 +3,8 @@
       <a href="HP.php" class="brand-logo"><img src="assets/Logo.jpeg"></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="VaS.php">View and Search</a></li>
-        <li><a href="Login.php">Login</a></li>
-        <li><a href="AR.php">Add/Remove</a></li>
+        <li><a href="Login.php">Sign in</a></li>
+        <li><a href="">Sign up</a></li>
       </ul>
     </div>
   </nav>
