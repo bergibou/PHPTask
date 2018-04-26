eb27ed9488a437eafce68bc92467ef8bb5a1b17f
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 00:28:55 +0200

    Create Signup.php

---
+++ Signup.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <?php
+                require 'headerlog.php';
+            ?>
+    </head>
+
+    <body>
+        <header>
+      <?php
+                require 'nav.php';
+            ?>
+      
+        
+            </header>
+        <center><h2>Login</h2>
+
+<form method="post" action="Login.php">
+  <div class="container">
+    <label for="pseudo"><b>Username</b></label>
+    <input type="text" placeholder="Create Username" name="pseudo" id="pseudo" required>
+
+    <label for="password"><b>Password</b></label>
+    <input type="password" placeholder="Create Password" name="password" id"password" required>
+        
+    <button type="submit">Login</button>
+  </div>
+</form>
+      <?php 
+	    
+  require 'connexion.php';
+  $pseudo=$_POST['pseudo'];
+  $password=$_POST['password'];
+  if (!isset($_POST['submit']))
+{
+	$sql ="INSERT INTO customers (customer_name,customer_password)VALUES('$pseudo','$password');	
+} 
+$conn->close();
+ require 'footer.php';
+	    
+            ?>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
