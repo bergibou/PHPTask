32e3f46c587df11823fe2d1037cca13a7672ba55
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 01:03:29 +0200

    Create newuser.php

---
+++ newuser.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost:3306";
+$username = "samy";
+$password = "S@4khadra92";
+$dbname = "samy_";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$username=$POST['pseudo'];
+$password=$POST['password'];
+	$sql = "INSERT INTO customers (customer_name, customer_password)
+        VALUES ('$username', '$password')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
