21ceb555058e233345b3645934e00c5ba2dc642a
Ganjibay <tebbalsamy@gmail.com>
2018-04-26 22:31:03 +0200

    Update AR.php

--- AR.php
+++ AR.php
@@ -1,20 +1,3 @@
-<?php
-
-$pseudo = (isset($_POST['pseudo']))?$_POST['pseudo']:'';
-$password =(isset($_POST['password']))?$_POST['password']:'';
-
- 
-$db = new PDO('mysql:host=localhost:3306;dbname=samy_;charset=utf8mb4', 'samy', 'S@4khadra92');
-$identi = $db->query("SELECT count(customer_id)as nbr FROM customers WHERE customer_name = '$pseudo' AND customer_password = '$password'");
-$row = $identi->fetch(PDO::FETCH_ASSOC);
-
-if($row['nbr'] != '1'){
-	header("Location: AR.php");
-}
-else{
-	echo "error username or password ";
-}
-?>
 <!DOCTYPE html>
   <html>
     <head>
