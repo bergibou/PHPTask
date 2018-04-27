d0c18888cd920f8801c69e3d7075fc22f5ac08e8
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 02:23:24 +0200

    Update addimage.php

--- addimage.php
+++ addimage.php
@@ -12,9 +12,9 @@ if ($conn->connect_error) {
 $TITLE=$_POST['title'];
 $DESCRIPTION=$_POST['Description'];
 $URL=$_POST['Url'];
-$session=['customer_id'];
+$ID=$_SESSION['Id'];
 	$sql = "INSERT INTO image (Title, description,url,customer_id)
-        VALUES ('$TITLE', '$DESCRIPTION','$URL','$session')";
+        VALUES ('$TITLE', '$DESCRIPTION','$URL','$ID')";
 if($conn->query($sql) === TRUE){
 echo "succeed";
 }
