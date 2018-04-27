4c298214f3b7ec2508441491e1b3f1dea9ff1619
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 08:08:41 +0200

    Update search.php

--- search.php
+++ search.php
@@ -12,20 +12,11 @@ if ($conn->connect_error) {
 $search=$_POST['search'];
 $sql = "SELECT image_id,Title,description,url FROM image WHERE (image_id LIKE '%$search%')";
  $res = $conn->query($sql);
-		echo '<div class="row container">'
 		 WHILE ($row = $res->fetch_assoc()){ 
-		 echo '<div class="col s4">
-                <div class="card">
-                    <div class="card-image">
-                        <IMG SRC="'.$row['url'].'">
-                    <span class="card-title">'.$row['Title'].'</span>
-		    </div>
-                    <div class="card-content">
-                        <p>Description : '.$row['description'].'</p>
-                    </div>
-                    </div>
-                </div>';
+		 
+                        echo '<IMG SRC="'.$row['url'].'">'
+                    $row['Title']
+                     $row['description']  
 		 }
-	echo '</div>'
 $conn->close();
 ?>
