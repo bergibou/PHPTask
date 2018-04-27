0e43fe49a7640c382ea7da16dd255417a3d5b71b
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 08:11:12 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,22 +1,58 @@
-<?php
-$servername = "localhost:3306";
-$username = "samy";
-$password = "S@4khadra92";
-$dbname = "samy_";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$search=$_POST['search'];
-$sql = "SELECT image_id,Title,description,url FROM image WHERE (image_id LIKE '%$search%')";
- $res = $conn->query($sql);
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <?php
+                require 'headerlog.php';
+            ?>    
+    </head>
+
+    <body>
+        <header>
+        <?php
+                require 'nav.php';
+            ?>
+            <form method="post" action="search.php">
+  
+  
+
+    <label for="text"><b>Search</b></label>
+    <input type="text" placeholder="Search here ..." name="search" id"search" required>
+        
+    <button type="submit">Search</button>
+  </div>
+</form>
+            </header>
+        <main>
+		<div class="row container">
+        <?php  
+		$servername = "localhost:3306";
+		$username = "samy";
+		$password = "S@4khadra92";
+		$dbname = "samy_";
+		 $conn = new mysqli($servername, $username, $password, $dbname);
+		 $sql = "SELECT * FROM image;";
+		 $res = $conn->query($sql);
 		 WHILE ($row = $res->fetch_assoc()){ 
-		 
-                        echo '<IMG SRC="'.$row['url'].'">'
-                    $row['Title']
-                     $row['description']  
-		 }
-$conn->close();
-?>
+		 echo '<div class="col s4">
+                <div class="card">
+                    <div class="card-image">
+                        <IMG SRC="'.$row['url'].'">
+                    <span class="card-title">'.$row['Title'].'</span>
+		    </div>
+                    <div class="card-content">
+                        <p>Description : '.$row['description'].'</p>
+                    </div>
+                    </div>
+                </div>';
+		 }?>
+	</div>
+        </main>
+        <?php
+                require 'footer.php';
+            ?>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
+
