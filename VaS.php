b9b42ec21283cd6ba5ccf70d1bf149f6a0469d6e
Ganjibay <tebbalsamy@gmail.com>
2018-04-27 07:02:20 +0200

    Update VaS.php

--- VaS.php
+++ VaS.php
@@ -28,7 +28,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-	$sql = "SELECT image_id,Title,descrition FROM `image`;";
+	$sql = "SELECT image_id,Title,descrition,url FROM `image`;";
                  WHILE ($row = $res->fetch_assoc()){ 
 		 echo '<div class="col s4">
                 <div class="card" id="size">
